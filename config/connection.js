692159dd0514b79160bd43f0a32616773c4f65cb
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-05-10 14:21:32 -0400

    Wrote the schema for the db and the connection file to connect to the db

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+const Sequelize = require("sequilize");
+require("dotenv").config();
+
+let sequilize;
+
+if (process.env.JAWSDB_URL) {
+  sequilize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequilize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_password,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
+
+module.exports = sequilize;
