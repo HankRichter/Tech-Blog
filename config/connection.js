a0fda5487e95db804557d86f7826f8bd932466ff
Hank Richter <hankrichter97@gmail.com>
2023-05-11 15:55:51 -0400

    added upper case to password in connection file

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
   sequilize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_password,
+    process.env.DB_PASSWORD,
     {
       host: "localhost",
       dialect: "mysql",
