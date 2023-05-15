c7deb72f05dc1adb9ae1e974bc15632b558390d0
Hank Richter <hankrichter97@gmail.com>
2023-05-15 15:46:16 -0400

    Wrote the get routes for the models.

---
+++ controllers/api/CommentRoutes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const Comment = require("../../models/Comment");
+
+router.get("/", async( req,res) => {
+    try {
+        const commentData = await Comment.findAll();
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
